df2990a0fad0ff586e3fcf210e6013defba473bf
Matthew Leach <matthew@mattleach.net>
2012-10-14 22:31:15 +0100

    init: Fix UP and initcall traversal.
    When executed, the traversal through the initcall() and UP list
    doesn't work as only "1" was being added to pointer offset, use a
    double pointer to get around that.
    
    Signed-off-by: Matthew Leach <matthew@mattleach.net>

--- setup/main.c
+++ setup/main.c
@@ -1,24 +1,27 @@
 #include <init.h>
 #include <up.h>
-#include <syscalls.h>
+#include <ksyscalls.h>
 #include <sched.h>
 
 void main()
 {
-	initcall_fn current_call = (initcall_fn)_initcall_begin;
-	initcall_fn last_call = (initcall_fn)_initcall_end;
-
-	// Call all inits.
-	while (current_call != last_call) {
-		current_call();
+	/*
+	 * Call kernel init calls.
+	 */
+	initcall_fn *current_call = (initcall_fn*)&_initcall_begin;
+	while (current_call != (initcall_fn*)&_initcall_end) {
+		(*current_call)();
 		current_call++;
 	}
 
-	// Fork user processes.
-	up *current_up = (up *)_up_begin;
-	while (current_up != (up*)_up_end) {
-		fork((unsigned int)current_up->u_fn,
+	/*
+	 * Fork user processes.
+	 */
+	up *current_up = (up *)&_up_begin;
+	while (current_up != (up *)&_up_end) {
+		_fork((unsigned int)current_up->u_fn,
 		     current_up->s_size);
+		current_up++;
 	}
 	_task_switch();
 }
