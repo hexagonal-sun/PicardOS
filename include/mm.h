77cfa2eb966722f96f9e647bc62164deb0b8c6e7
Matthew Leach <matthew@mattleach.net>
2012-11-23 21:58:15 +0000

    mm: Add memory types
    To distinguish between VAs and PAs, create two types: vm_offset_t for
    VAs and phys_addr_t for PAs.
    
    Signed-off-by: Matthew Leach <matthew@mattleach.net>

--- include/mm.h
+++ include/mm.h
@@ -1,5 +1,8 @@
 /*
- * Memory maintenance functions.
+ * Memory maintenance functions and types.
  */
+
+typedef unsigned long vm_offset_t;
+typedef unsigned long phys_addr_t;
 
 void *malloc(unsigned int sz);
