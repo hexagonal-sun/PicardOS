8009fcec908d5fe2039c82d6920235e237d6319c
Matthew Leach <matthew@mattleach.net>
2012-11-25 15:18:52 +0000

    mm: Add pmap and vm_mapping structs
    In order to keep track of the mapping that occur with VM, create two
    structs that maintain this information.
    
    Signed-off-by: Matthew Leach <matthew@mattleach.net>

--- include/mm.h
+++ include/mm.h
@@ -2,7 +2,29 @@
  * Memory maintenance functions and types.
  */
 
+#include <pgte.h>
+
 typedef unsigned long vm_offset_t;
 typedef unsigned long phys_addr_t;
+
+phys_addr_t mem_start;
+
+struct vm_mapping
+{
+	pt_entry_t *page;
+	phys_addr_t pa;
+	vm_offset_t va;
+	char *name;
+	struct vm_mapping *next;
+};
+
+struct pmap
+{
+	pt_entry_t *base; /* Page list */
+	struct pmap *next;/* Next mapping */
+	struct vm_mapping *mappings; /* List of mappings */
+};
+
+extern struct pmap kernel_pmap;
 
 void *malloc(unsigned int sz);
