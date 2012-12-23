e5a5008fe9862c9d9187ebf92177d1b076666ff1
Matthew Leach <matthew@mattleach.net>
2012-12-23 19:25:20 +0000

    mm: Add kernel virtual memory.
    The kernel currently is linked at an address that is within the
    physical RAM of the platform. Ideally we would like the kernel to be
    linked at a virtual address and use page tables so we don't have to
    worry about changing the link address.
    
    This patch enables virtual memory on an ARMv7 platform and plants the
    kernel at a virtual "low memory" address.
    
    Signed-off-by: Matthew Leach <matthew@mattleach.net>

--- setup/init.s
+++ setup/init.s
@@ -1,4 +1,5 @@
 	.set CPSR_USER, 0x10
+	.set PAGE_MASK, 0xfffff
 	.set CPSR_SYS, 0x1F
 	.set CPSR_SUP, 0x13
 	.set CPSR_IRQ, 0x12
@@ -6,59 +7,72 @@
 	.global _vec_start
 	.global _vec_end
 _start:
-	mov r12, pc
-_vec_start:
-	LDR pc, [pc, #24]
-	LDR pc, [pc, #24]
-	LDR pc, [pc, #24]
-	LDR pc, [pc, #24]
-	LDR pc, [pc, #24]
-	LDR pc, [pc, #24]
-	LDR pc, [pc, #24]
-	LDR pc, [pc, #24]
-	.word    __setup                 @ reset
-	.word    .                       @ undef
-	.word    __syscall               @ svc
-	.word    .                       @ pabort
-	.word    .                       @ dabort
-	.word    .                       @ hyp
-	.word    __irq_entry             @ irq
-	.word    .                       @ fiq
+	mov	r12, pc		@ Save the start address
+	ldr	r0,=PAGE_MASK
+	bic	r12, r12, r0		@ Page align the star addr ptr
 
-_vec_end:
-	.align 8
+	adr	sp, __boot_stack	@ Set the boot stack
 
-__setup:
-	/* Initialise memory */
-	MOV R0, #0
-	LDR R1, =PCB_HEAD
-	STR R0, [R1]
-	LDR R1, =SHOULD_CTX_SWITCH
-	STR R0, [R1]
+	mov	r0, r12
+	ldr	r1, =0xc0000000	@ Load the linked kernel address
+	bl	setup_pg_tables
 
-	/* set the supervisor stack */
-	LDR sp, =svc_stack_end
+	mov	r6, r0
+	mov	r0, #0
+	mcr	p15, 0, r0, c7, c10, 4  @ drain write buffer
+	mcr	p15, 0, r0, c8, c7, 0   @ flush I,D TLBs
+	mrc	p15, 0, r0, c1, c0, 0   @ read control reg
+	bic	r0, r0, #1 << 28        @ clear SCTLR.TRE
+	orr	r0, r0, #0x5000         @ I-cache enable, RR cache replacement
+	orr	r0, r0, #0x003c         @ write buffer
+	orr	r0, r0, #1              @ MMU enabled
+	mov	r1, #0xfffffffd         @ domain 0 = client
+	bic	r6, r6, #1 << 31        @ 32-bit translation system
+	bic	r6, r6, #3 << 0         @ use only ttbr0
+	mcr	p15, 0, r3, c2, c0, 0   @ load page table pointer
+	mcr	p15, 0, r1, c3, c0, 0   @ load domain access control
+	mcr	p15, 0, r6, c2, c0, 2   @ load ttb control
+	mcr	p15, 0, r0, c7, c5, 4   @ ISB
+	mcr	p15, 0, r0, c1, c0, 0   @ load control register
+	mrc	p15, 0, r0, c1, c0, 0   @ and read it back
+	mov	r0, #0
+	mcr	p15, 0, r0, c7, c5, 4   @ ISB
 
-	/* Switch to IRQ Mode */
-	MSR CPSR_c, #CPSR_IRQ
+	mov	r0, r12		@ Load the kernel PA
+	ldr	r1, =0xc0000000	@ Set the kernel linked VA
+	mov	r2, r6			@ Load the kernel pdp
 
-	/* Set the IRQ stack */
-	LDR sp, =irq_stack_end
+	ldr	sp, =__boot_stack	@ Set the VA of the temporary stack.
+	ldr	r3, =post_mmu_setup	@ Load the VA of the fn
+	ldr	lr, =1f			@ Set the virtual return address
+	mov	pc, r3			@ Branch to the fn
 
-	/* Enable interupts */
-	MOV R0, #1
-	LDR R1, =IRQ_ENABLE
-	STR R0, [R1]
+1:
+	mov	sp, r0
 
-	/* Switch back to sup mode for kernel
-	   initalisation */
-	MSR CPSR_c, #CPSR_SUP
-
-	LDR R0, =main
+	LDR R0, =main			@ Jump to C code.
 	mov PC, R0
 
-	B  .	@We should never get here.
-	.section ".stack"
-svc_stack_begin:	.space	4096
-svc_stack_end:		.space 4096
-irq_stack_end:
+	b	.			@ We should never get here.
+	.ltorg
+	.space	512
+__boot_stack:
+	.section ".vectors"
+_vec_start:
+	ldr	pc, [pc, #24]
+	ldr	pc, [pc, #24]
+	ldr	pc, [pc, #24]
+	ldr	pc, [pc, #24]
+	ldr	pc, [pc, #24]
+	ldr	pc, [pc, #24]
+	ldr	pc, [pc, #24]
+	ldr	pc, [pc, #24]
+	.word	.                       @ reset
+	.word	.                       @ undef
+	.word	__syscall               @ svc
+	.word	.                       @ pabort
+	.word	.                       @ dabort
+	.word	.                       @ hyp
+	.word	__irq_entry             @ irq
+	.word	.                       @ fiq
+_vec_end:
---
+++ setup/mem_init.c
@@ -0,0 +1,111 @@
+#include <pgte.h>
+#include <mm.h>
+
+#define KERNEL_STACK 0xF0000000
+
+struct pmap kernel_pmap;
+
+/*
+ * At this time, we don't have the malloc() function setup so declare
+ * these kernel mappings statically.
+ */
+struct vm_mapping kernel_code;
+struct vm_mapping kernel_stack;
+
+
+/**
+ * This function setups up the kernel page tables to map to the link
+ * address.
+ *
+ * @param mem_start, the start of memory that should be page aligned.
+ * @param kern_va, The page aligned link address.
+ * @returns the PA of the page tables to be put in TTBR1/0.
+ * @note This function should be called with the MMU off and with a
+ * temporary stack set.
+ */
+pt_entry_t *setup_pg_tables(phys_addr_t mem_start, vm_offset_t kern_va)
+{
+	pt_entry_t section = 0;
+	pt_entry_t *pdp = (pt_entry_t*)(mem_start + 0x4000);
+	unsigned int kmem = inclin(0);
+	unsigned int current_phys_addr = 0;
+
+	for (unsigned int i = 0; i < SC_MASK; i++)
+		pdp[i] = section;
+
+	/*
+	 * Setup the section to point to point to the PA of the kernel
+	 * code.
+	 */
+	section = 2;
+	section |= mem_start;
+	section |= ARM_SC_BUF | ARM_SC_CACHE;
+	section |= ARM_SC_AP_0 | ARM_SC_AP_1;
+
+	/*
+	 * Map to the kernel link address.
+	 */
+	pdp[lin2pgidx(kern_va)] = section;
+
+	/*
+	 * Create a 1:1 transition mapping.
+	 */
+	pdp[lin2pgidx(mem_start)] = section;
+
+	return pdp;
+}
+
+vm_offset_t post_mmu_setup(phys_addr_t mem_start,
+			   vm_offset_t kern_va,
+			   phys_addr_t pdp_pa)
+{
+	pt_entry_t section;
+	pt_entry_t *pdp;
+
+	mem_start &= ~PAGE_MASK;
+	kern_va &= ~PAGE_MASK;
+
+	/*
+	 * Now the pdp will be the PA of the tables.  We need to
+	 * convert that to a VA before we remove the transition
+	 * mapping.
+	 */
+	vm_offset_t pdp_va = pdp_pa + (kern_va - mem_start);
+	pdp = (pt_entry_t *)pdp_va;
+
+	/*
+	 * Remove the transition mapping.
+	 */
+	pdp[lin2pgidx(mem_start)] = 0;
+
+
+	/*
+	 * Map a page for the kernel stack.
+	 */
+	section = 2;
+	section |= inclin(mem_start);
+	section |= ARM_SC_BUF | ARM_SC_CACHE;
+	section |= ARM_SC_AP_0 | ARM_SC_AP_1;
+	pdp[lin2pgidx(KERNEL_STACK)] = section;
+
+	/*
+	 * Setup the kernel mapping structs.
+	 */
+	kernel_code.page = &pdp[lin2pgidx(kern_va)];
+	kernel_code.pa = mem_start;
+	kernel_code.va = kern_va;
+	kernel_code.name = "Kernel code";
+	kernel_code.next = &kernel_stack;
+
+	kernel_stack.page = &pdp[lin2pgidx(inclin(kern_va))];
+	kernel_stack.pa = inclin(mem_start);
+	kernel_stack.va = inclin(kern_va);
+	kernel_stack.name = "Kernel stack";
+	kernel_stack.next = 0;
+
+	kernel_pmap.base = pdp;
+	kernel_pmap.next = 0;
+	kernel_pmap.mappings = &kernel_code;
+
+	return KERNEL_STACK | PAGE_MASK;
+}
