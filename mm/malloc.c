2fe695e68084f30583a7eb88805a1e64b24ba6f4
Matthew Leach <matthew@mattleach.net>
2013-02-08 22:26:46 +0000

    mm: use the data_start page for malloc
    Malloc can now use the mapped kernel data page for allocation rather
    than using the end of the binary.
    
    Signed-off-by: Matthew Leach <matthew@mattleach.net>

--- mm/malloc.c
+++ mm/malloc.c
@@ -1,14 +1,13 @@
 #include <mm.h>
 #include <init.h>
 
-extern unsigned int _edata;
 char *malloc_ptr;
 
 void mem_init()
 {
-	malloc_ptr = (char *)&_edata;
+	malloc_ptr = (char *)data_start;
 }
-early_initcall(mem_init);
+system_initcall(mem_init);
 
 void *malloc(unsigned int sz)
 {
